17f0b2e16012ab8eb9bf8f7ca436597226c1f585
TiciB <tebezerra@gmail.com>
2022-06-05 21:34:14 -0300

    Configurando servidor

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello, Tici");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({status: 'success'}).status(200);
+});
+
+app.listen(3000, () =>{
+    console.log ('servidor funcionando');
+});
